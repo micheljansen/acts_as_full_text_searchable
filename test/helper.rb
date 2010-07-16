09a12420414e20887e29eb9f3ce066b6cb2255d5
micheljansen <mail@micheljansen.org>
2010-07-16 10:48:15 +0200

    Initial commit to acts_as_full_text_searchable.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'acts_as_full_text_searchable'
+
+class Test::Unit::TestCase
+end
